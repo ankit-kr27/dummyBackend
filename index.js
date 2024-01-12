e444c06081fc621eba77dea8fd776fe44e70d2d5
Ankit Kumar <98731902+ankit-kr27@users.noreply.github.com>
2024-01-12 20:59:54 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// console.log("Hello World")
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res)=>{
+    res.send("Hello World");
+})
+
+app.get('/twitter', (req, res)=>{
+    res.send('ank_it_kr')
+})
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Example app listening on port ${port}`)
+})
